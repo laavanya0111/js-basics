d86353d23330b1435ecb9a610322a7843e976479
Laavanya Goel <laavanyagoel01@gmail.com>
2021-09-02 19:29:02 +0530

    console basics

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hey this is my first code of js")
+console.log(124904567)
+console.log({name:"Laavanya"})
+console.log(true)
+console.log(false)
+
+console.warn("This is a warning")
+console.error("This is error")
