c4ac4d8c54d31035ce3d66e377ea54cce8d5b462
Laavanya Goel <laavanyagoel01@gmail.com>
2021-09-03 19:50:32 +0530

    Assignment operator

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
 
-// Arithmetic Operator
-const a = 2
-const b = 2
-let c = 0
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a**b)
-console.log(b % a)
-
-console.log('-----------------------------')
-c += 1
-console.log(c)
+// Asignment Operator
+let name = 'Sakshi'
+ let a = 9
