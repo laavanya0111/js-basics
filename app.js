a19c4b357f755210503554e425fb7491d9751474
Laavanya Goel <laavanyagoel01@gmail.com>
2021-09-08 21:52:14 +0530

    update selectors

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 
 const element = document.querySelector('#selectme')
-console.log(element)
+//element.innerHTML = "This is from JS"
+//element.style.color = 'cyan'
+//element.style.fontsize = '40px'
+//element.style = "display:none"
+
+//console.log(element.classList)
+//element.classList.add('five')
+//console.log(element.classList)
+//element.classList.remove('one')
+//console.log(element.classList)
+
+const value = element.classList.contains('three')
+console.log(value)
