ba36e08645030d70e20e9d16930b3188f7958972
Laavanya Goel <laavanyagoel01@gmail.com>
2021-09-08 22:51:04 +0530

    event selectors

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-
-const element = document.querySelector('#selectme')
-//element.innerHTML = "This is from JS"
-//element.style.color = 'cyan'
-//element.style.fontsize = '40px'
-//element.style = "display:none"
-
-//console.log(element.classList)
-//element.classList.add('five')
-//console.log(element.classList)
-//element.classList.remove('one')
-//console.log(element.classList)
-
-const value = element.classList.contains('three')
-console.log(value)
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+console.log(button,body)
+button.addEventListener('click', () => {
+    body.classList.toggle('dark')
+})
