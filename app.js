9dab2cb17db91c329187024691dd06678241f58d
Laavanya Goel <laavanyagoel01@gmail.com>
2021-09-09 15:54:26 +0530

    function keyword

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
+//const rock = document.querySelector('#rock')
+//const paper = document.querySelector('#paper')
+//const scissor = document.querySelector('#scissor')
+
+function logName(Name="John Doe", age=18) {
+    console.log(Name + " You are" + age)
+}
+
+function add(a=0, b=0){
+    return a + b
+}
+
+const result = add()
+console.log(result)
