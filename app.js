4051dcd792ba08570c2860ca88cca2ce35ebd5f5
Laavanya Goel <laavanyagoel01@gmail.com>
2021-09-09 16:38:01 +0530

    ES-6 functions

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ function logName(Name="John Doe", age=18) {
     console.log(Name + " You are" + age)
 }
 
-function add(a=0, b=0){
+const add = (a=0, b=0) => {
     return a + b
 }
 
-const result = add()
-console.log(result)
+console.log(add(12,23))
